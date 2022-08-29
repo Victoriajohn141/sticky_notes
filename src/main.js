7596c3d8d8a9d1f44c4501446b26f190dc78c5d7
Victoria John <vicjohn241@gmail.com>
2022-08-29 14:03:38 +0100

    created a noteList function, a container for the note data structure.
    created a create newnote function, a function that allows you create new note
    
    created a deletenote function that allows you delete notes
    
    created an update note function that allows you update
    old content to newcontent.

--- src/main.js
+++ src/main.js
@@ -1,30 +1,94 @@
 "use strict"
+const noteList = [];
 
-function createNewNote() {
-  console.log('Calling function createNewNote')
-const notesContainer = document.querySelector(".notes");
+let firstNote = {text: 'i need to buy milk'};
 
-const addNoteButton = notesContainer.querySelector(".add-note");
+ noteList.push(firstNote);
+ 
+ console.log(firstNote);
+ console.log(noteList);
 
-createNewNote().forEach((note) => {
-  const noteElement = createNoteElement(note.class, note.content);
-  notesContainer.insertBefore(noteElement, addNoteButton);
-});
 
+function createNewNote(text) {
+ let secondNote = [];
+
+ let note = {text: ''}
+
+ secondNote.push(note)
+
+ return text;
+
+ console.log(text)
 }
-const noteContainer = document.querySelector(".notes");
-  // const addNoteButton = document.querySelector(".add-note");
+
+function createNewNoteV2(value) {
+    let note2 = {text: value}
+
+    console.log(value)
+    return note2
+}
+
+console.log(createNewNoteV2('new note')) // {text: 'new note'}
+console.log(createNewNoteV2('another note'))
+
+
+
+
+
+// const noteContainer = document.querySelector(".notes");
+//   // const addNoteButton = document.querySelector(".add-note");
   
-   const noteElement = document.createElement("textarea");
+//    const noteElement = document.createElement("textarea");
 
-function addNote() {   
+// addNote takes in string, creates a note then adds it into the note container
+function addNote(value) {   
+ // 1. create note data structure from value (input parameter)
+ // 2. add it to the noteList
+ let addNote = createNewNoteV2(value);
+ noteList.push(addNote)
+ 
+
+}
+
+// updateNote edits the data structure at the index passed with newContent
+// 
+function updateNote(index, newContent) {
+ 
+//  let objIndex = noteList.findIndex((obj => obj.index == 1));
+
+ console.log("before update: ", noteList[index])
+
+ noteList[index].text = newContent;
+
+ console.log("after update: ", noteList[index])
 
 }
 
-function updateNote(id, newContent) {
+updateNote(0, 'changed')
+console.log(noteList)
+
+
+// deleteNote removes an item from the noteList vy index
+function deleteNote(index) {
+
+ let removeItem = noteList.splice(index, 1)
+
+ console.log('removed: ', removeItem)
 
 }
 
-function deleteNote() {
+deleteNote(0)
+console.log(noteList)
+// console.log('Calling function createNewNote')
+// const notesContainer = document.querySelector(".notes");
 
-}
+// const addNoteButton = notesContainer.querySelector(".add-note");
+
+// createNewNote().forEach((note) => {
+//   const noteElement = createNoteElement(note.class, note.content);
+//   notesContainer.insertBefore(noteElement, addNoteButton);
+// });
+
+console.log(addNote('i did something today'))
+addNote('testing microphone')
+console.log(noteList)
